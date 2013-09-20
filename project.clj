54fe6dcfce0936d2dd04755e6345d55ee90351bd
ryan spangler <ryan.spangler@gmail.com>
2013-09-19 19:02:45 -0700

    version

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject caribou/butterfly "0.0.7"
+(defproject caribou/butterfly "0.1.0"
   :description "Flying animal of the social world"
   :url "http://github.com/caribou/butterfly"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [caribou/twitter-api "0.7.5"]
-                 [caribou/instagram-api "0.1.7"]])
+                 [caribou/instagram-api "0.1.8"]])
